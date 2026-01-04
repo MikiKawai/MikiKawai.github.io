9a4ecb4a317cadf8cd61bf8f748ab104ba25be92
MikiKawai <zhaokuii11@163.com>
2026-01-05 01:46:58 +0800

    笔记

--- StyleSources/diary.js
+++ StyleSources/diary.js
@@ -2,6 +2,11 @@ const diary = [
     //头像地址，日期，文字数组，图片地址数组
     [
         'https://bkimg.cdn.bcebos.com/pic/6f061d950a7b02087bf4e27f4392e5d3572c11df6253?x-bce-process=image/format,f_auto/watermark,image_d2F0ZXIvYmFpa2UyNzI,g_7,xp_5,yp_5,P_20/resize,m_lfit,limit_1,h_1080',
+        "2026-1-5 1：39",
+        ['<font color=#FF0080 size="5">我好想你，可你有这么绝情，事情宁愿问别人问ai也不会问对方了，我没有理由继续纠缠下去了，遗憾那就遗憾！</font>'],
+    ],
+    [
+        'https://bkimg.cdn.bcebos.com/pic/6f061d950a7b02087bf4e27f4392e5d3572c11df6253?x-bce-process=image/format,f_auto/watermark,image_d2F0ZXIvYmFpa2UyNzI,g_7,xp_5,yp_5,P_20/resize,m_lfit,limit_1,h_1080',
         "2025-12-31 18:15",
         ['<font color=#FF0080 size="5">那就互不打扰，再见了。删除我也是给别人腾出位置，你说你累了说我后面没有给你爱，你自己呢？你自己同样没有给予。我给不了你什么？你自己跟我谈的时候也清楚啊，你一直不开心，我也有。为什么不能好好的交流。那就这样吧。。。</font>'],
     ],
